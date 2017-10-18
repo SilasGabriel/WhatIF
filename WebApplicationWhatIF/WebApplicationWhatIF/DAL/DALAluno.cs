78fc8d50653c9fc85be528878e509560bde922cb
Silas Gabriel Barbosa de Sousa <silas.gabriel@academico.ifrn.edu.br>
2017-10-18 17:53:39 -0300

    Editar Perfil em Andamento

--- WebApplicationWhatIF/WebApplicationWhatIF/DAL/DALAluno.cs
+++ WebApplicationWhatIF/WebApplicationWhatIF/DAL/DALAluno.cs
@@ -99,5 +99,91 @@ namespace WebApplicationWhatIF.DAL
             }
             return aux;
         }
+
+        //Select
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public List<Modelo.Aluno> Select(string nome)
+        {
+            Modelo.Aluno DALaluno;
+            // Cria Lista Vazia
+            List<Modelo.Aluno> DALlistAlu = new List<Modelo.Aluno>();
+            // Cria Conexão com banco de dados
+            SqlConnection conn = new SqlConnection(connectionString);
+            // Abre conexão com o banco de dados
+            conn.Open();
+            // Cria comando SQL
+            SqlCommand cmd = conn.CreateCommand();
+            // define SQL do comando
+            cmd.CommandText = "Select * from Aluno Where nome = @nome";
+            cmd.Parameters.AddWithValue("@nome", nome);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+
+                while (dr.Read()) // Le o proximo registro
+                {
+                    // Cria objeto com dados lidos do banco de dados
+                    DALaluno = new Modelo.Aluno(
+                        dr["nome"].ToString(),
+                        dr["senha"].ToString(),
+                        dr["email"].ToString(),
+                        Convert.ToBoolean(dr["escolaPublica"]),
+                        Convert.ToBoolean(dr["aministrador"])
+                        );
+                    // Adiciona a disciplina lida à lista
+                    DALlistAlu.Add(DALaluno);
+                }
+            }
+            // Fecha DataReader
+            dr.Close();
+            // Fecha Conexão
+            conn.Close();
+
+            return DALlistAlu;
+        }
+        
+        //SelectAll
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public List<Modelo.Aluno> SelectAll()
+        {
+            // Variavel para armazenar um livro
+            Modelo.Aluno aAluno;
+            // Cria Lista Vazia
+            List<Modelo.Aluno> aListAluno = new List<Modelo.Aluno>();
+            // Cria Conexão com banco de dados
+            SqlConnection conn = new SqlConnection(connectionString);
+            // Abre conexão com o banco de dados
+            conn.Open();
+            // Cria comando SQL
+            SqlCommand cmd = conn.CreateCommand();
+            // define SQL do comando
+            cmd.CommandText = "Select * from Aluno";
+            // Executa comando, gerando objeto DbDataReader
+            SqlDataReader dr = cmd.ExecuteReader();
+            // Le titulo do livro do resultado e apresenta no segundo rótulo
+            if (dr.HasRows)
+            {
+
+                while (dr.Read()) // Le o proximo registro
+                {
+                    // Cria objeto com dados lidos do banco de dados
+                    aAluno = new Modelo.Aluno(
+                        dr["nome"].ToString(),
+                        dr["senha"].ToString(),
+                        dr["email"].ToString(),
+                        Convert.ToBoolean(dr["escolaPublica"]),
+                        Convert.ToBoolean(dr["aministrador"])
+                        );
+                    // Adiciona o livro lido à lista
+                    aListAluno.Add(aAluno);
+                }
+            }
+            // Fecha DataReader
+            dr.Close();
+            // Fecha Conexão
+            conn.Close();
+
+            return aListAluno;
+        }
     }
 }
